48ac4d4757086104bbc40a79fe1832f65f22180d
张沈鹏 <zsp007@gmail.com>
2012-06-08 16:18:34 +0800

    fix

--- publish.sh
+++ publish.sh
@@ -1,3 +1,5 @@
-find  js/ -name "*.js"  -exec uglifyjs --overwrite {} \;
+rm js -rf
+cp js_dev js -R
+find  js/ -name "*.js"  -exec uglifyjs -nc --overwrite {} \;
 
 
--- run_dev.sh
+++ run_dev.sh
@@ -1,6 +1,6 @@
 PREFIX=$(cd "$(dirname "$0")"; pwd)
 mkdir -p $PREFIX/js
-jitter $PREFIX/coffee/ $PREFIX/js/
+jitter $PREFIX/coffee/ $PREFIX/js_dev/
  
 
 
