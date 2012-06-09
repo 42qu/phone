cec9dd5c6c6990ff840502d90e29642097522c5c
张沈鹏 <zsp007@gmail.com>
2012-06-09 11:57:45 +0800

    fix

---
+++ js_dev/requirejs.js
@@ -0,0 +1,31 @@
+(function() {
+
+  requirejs.config({
+    shim: {
+      backbone: {
+        deps: ['underscore', 'jquery']
+      },
+      jquery_mobile: ['jquery'],
+      jquery_tmpl: ['jquery'],
+      index: ['jquery_mobile', 'jquery_tmpl', 'backbone']
+    },
+    paths: {
+      jquery: '/js/jquery/1.7.2',
+      underscore: '/js/underscore/1.3.3',
+      jquery_mobile: '/js/jquery.mobile/1.1.0',
+      jquery_tmpl: '/js/jquery.tmpl/1.0',
+      backbone: '/js/backbone/0.9.2',
+      index: '/js_dev/index'
+    }
+  });
+
+  require(['jquery', 'underscore', 'jquery_mobile', 'jquery_tmpl', 'backbone', 'index'], function($) {
+    return $(document).bind("mobileinit", function() {
+      $.mobile.ajaxEnabled = false;
+      $.mobile.linkBindingEnabled = false;
+      $.mobile.hashListeningEnabled = false;
+      return $.mobile.pushStateEnabled = false;
+    });
+  });
+
+}).call(this);
